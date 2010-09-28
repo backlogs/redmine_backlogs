15418e5b0e4b5c3f39cf2c47eee2aa5c4d3035ba
friflaj <friflaj@ajellito.com>
2010-09-29 00:28:57 +0200

    Just 2 more tests

--- features/step_definitions/_then_steps.rb
+++ features/step_definitions/_then_steps.rb
@@ -117,7 +117,12 @@ Then /^the sprint should be updated accordingly$/ do
   
   sprint.attributes.each_key do |key|
     unless ['updated_on', 'created_on'].include?(key)
-      (key.include?('_date') ? sprint[key].strftime("%Y-%m-%d") : sprint[key]).should == @sprint_params[key]
+      if key.include?('_date')
+        "#{sprint[key]}".should == "#{@sprint_params[key]}"
+      else
+        sprint[key].should == @sprint_params[key]
+      end
+      #(key.include?('_date') ? sprint[key].strftime("%Y-%m-%d") : sprint[key]).should == @sprint_params[key]
     end
   end
 end
