978ff54631789374c9cd839e0442679956d6bbb5
Masatomo Nakano <masatomon@reallyenglish.com>
2010-10-14 13:42:47 +0100

    errros.add adds error_ prefix. this causes not found label error

--- app/models/task.rb
+++ app/models/task.rb
@@ -103,7 +103,7 @@ class Task < Issue
   
   def validate_blocks_list(list)
     if list.split(/\D+/).length==0
-      errors.add :label_blocks, :error_must_have_comma_delimited_list
+      errors.add :label_blocks, :must_have_comma_delimited_list
       false
     else
       true
