77716c5c25eefb1e19ccdd566519b2125a3f107b
Frank Blendinger <fb@intoxicatedmind.net>
2010-11-09 16:13:46 +0100

    move releases to own table

--- app/models/release.rb
+++ app/models/release.rb
@@ -3,10 +3,12 @@ require 'date'
 class Release < Project
     unloadable
 
+    belongs_to :project
+
     validate :start_and_end_dates
 
     def start_and_end_dates
-        errors.add_to_base("Sprint cannot end before it starts") if self.release_start_date && self.release_end_date && self.release_start_date >= self.release_end_date
+        errors.add_to_base("Release cannot end before it starts") if self.start_date && self.end_date && self.start_date >= self.end_date
     end
 
 end
