943ee763e297191077e0cba5b010bb7fbb011940
Frank Blendinger <fb@intoxicatedmind.net>
2010-11-10 14:50:39 +0100

    release does not inherit from project (models)

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,6 +1,6 @@
 require 'date'
 
-class Release < Project
+class Release < ActiveRecord::Base
     unloadable
 
     belongs_to :project
@@ -8,7 +8,7 @@ class Release < Project
     validate :start_and_end_dates
 
     def start_and_end_dates
-        errors.add_to_base("Release cannot end before it starts") if self.start_date && self.end_date && self.start_date >= self.end_date
+        errors.add_to_base("Release cannot end before it starts") if self.release_start_date && self.release_end_date && self.release_start_date >= self.release_end_date
     end
 
 end
