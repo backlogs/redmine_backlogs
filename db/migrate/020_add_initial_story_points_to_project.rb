5d4fb64605572735a21eada7b2fe3ebdf541a15c
Frank Blendinger <fb@intoxicatedmind.net>
2010-11-18 23:03:27 +0200

    remove old db migrations, add unified new one

--- db/migrate/020_add_initial_story_points_to_project.rb
+++
@@ -1,9 +0,0 @@
-class AddInitialStoryPointsToProject < ActiveRecord::Migration
-  def self.up
-    add_column :projects, :initial_story_points, :integer, :null => true
-  end
-
-  def self.down
-    remove_column :projects, :initial_story_points
-  end
-end
--- db/migrate/022_add_name_to_releases.rb
+++
@@ -1,9 +0,0 @@
-class AddNameToReleases < ActiveRecord::Migration
-  def self.up
-    add_column :releases, :name, :string, :null => false
-  end
-
-  def self.down
-    remove_column :releases, :name
-  end
-end
