ed1397b18093ac7813bfb9c574625f836aba8c1c
friflaj <friflaj@redminebacklogs.net>
2011-03-27 19:52:41 +0200

    Reference to moved template, translation status to website

--- lib/tasks/check-translations.rb
+++ lib/tasks/check-translations.rb
@@ -2,32 +2,78 @@
 
 require 'yaml'
 
+webdir = File.join(File.dirname(__FILE__), '..', '..', '..', 'www.redminebacklogs.net')
+$logfile = nil
+if File.directory? webdir
+    $logfile = File.open(File.join(webdir, '_posts', 'en', '1992-01-01-translations.textile'), 'w')
+end
+
+def log(s)
+    puts s
+    $logfile.puts(s) if $logfile
+end
+
 langdir = File.join(File.dirname(__FILE__), '..', '..', 'config', 'locales')
 
 template_file = "#{langdir}/en.yml"
 template = YAML::load_file(template_file)['en']
 
-errors = false
+log """---
+title: Translations
+layout: default
+categories: en
+---
+h1. Translations
 
-Dir.glob("#{langdir}/*.yml").each {|lang_file|
+h2. en
+
+serves as a base for all other translations
+
+"""
+
+Dir.glob("#{langdir}/*.yml").sort.each {|lang_file|
   next if lang_file == template_file
+
 
   lang = YAML::load_file(lang_file)
   l = lang.keys[0]
 
+
+  missing = []
+  obsolete = []
+  varstyle = []
+
   template.each_pair {|key, txt|
-    if ! lang[l][key]
-      puts "missing: ${l}: #{key}"
-      errors = true
+    missing << key if ! lang[l][key]
+  }
+
+  lang[l].keys.each {|key|
+    if !template[key]
+      obsolete << key
+    elsif lang[l][key].include?('{{')
+      varstyle << key
     end
   }
 
-  lang[l].keys.each {|k|
-    if !template[k]
-      puts "obsolete: #{l}: #{k}"
-      errors = true
-    end
-  }
+  if missing.size > 0 || obsolete.size > 0
+    columns = 2
+    log "h2. #{l}: needs update\n\n"
+    [[missing, 'Missing'], [obsolete, 'Obsolete'], [varstyle, 'Old-style variable substitution']].each {|cat|
+        keys, title = *cat
+        next if keys.size == 0
+
+        log "*#{title}*\n\n"
+        keys.sort!
+        while keys.size > 0
+            row = (keys.shift(columns) + ['', ''])[0..columns-1]
+            log "|" + row.join("|") + "|\n"
+        end
+        log "\n"
+    }
+  else
+    log "h2. #{l}\n\n"
+  end
 }
 
-puts "All OK!" unless errors
+
+$logfile.close if $logfile
