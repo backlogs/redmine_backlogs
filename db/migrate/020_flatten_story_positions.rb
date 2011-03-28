b8777a1b00ecea58fc393af1e22275c735e08b44
friflaj <friflaj@ajellito.com>
2011-03-28 23:25:49 +0200

    stupid "Attempted to update a stale object" errors

--- db/migrate/020_flatten_story_positions.rb
+++ db/migrate/020_flatten_story_positions.rb
@@ -1,27 +1,13 @@
 class FlattenStoryPositions < ActiveRecord::Migration
   def self.up
-    errors = []
-    Issue.find(:all, :conditions => "subject is NULL or (start_date is null and not due_date is null) or start_date > due_date or updated_on < created_on").each do |issue|
-      errors << issue.id
-    end
+    # stupid "Attempted to update a stale object" errors
 
-    if errors.size > 0
-      puts "You have issues in your database that have one or more of the following problems:"
-      puts "* subject is empty or NULL"
-      puts "* due date is set, but start date is not"
-      puts "* start date is later than due date"
-      puts "* updated-date is before created-date"
-      raise "Please fix the issues with the follwing IDs and retry the migration"
-      errors.each {|id|
-        puts "- #{id}"
-      }
-    end
+    # this makes sure everything has an unique id that won't conflict
+    # with what we're about to do next
+    execute "update issues set position = -id"
 
-    ActiveRecord::Base.transaction do
-      Story.find(:all, :order => "project_id ASC, fixed_version_id ASC, position ASC").each_with_index do |s,i|
-        s.position=i+1
-        s.save!
-      end
+    Story.find(:all, :order => "project_id ASC, fixed_version_id ASC, position ASC").each_with_index do |s,i|
+      execute "update issues set position=#{i+1} where id=#{s.id}"
     end
   end
 
