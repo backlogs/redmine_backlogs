298edfd0a6bc2ca77ec63c08e8ad08aa22baf2d6
stevel <stevel@digiwell.com.tw>
2011-05-23 14:56:44 +0800

    Fix #82, Card labels could not be fetched. no such file to load -- open-uri/cached

--- lib/cards.rb
+++ lib/cards.rb
@@ -6,25 +6,25 @@ require 'rexml/document'
 
 require 'yaml'
 require 'uri/common'
-require 'open-uri/cached'
+#require 'open-uri/cached'
 
 module Cards
     class TaskboardCards
         include Redmine::I18n
         include GravatarHelper::PublicMethods
         include ERB::Util
-                
+
         begin
           LABELS = YAML::load_file(File.dirname(__FILE__) + '/labels.yaml')
         rescue
           LABELS = {}
         end
-        
+
         def self.selected_label
             return nil if not Setting.plugin_redmine_backlogs[:card_spec]
             return LABELS[Setting.plugin_redmine_backlogs[:card_spec]]
         end
-    
+
         def initialize(lang)
             set_language_if_valid lang
 
@@ -36,23 +36,23 @@ module Cards
 
             geom = Prawn::Document::PageGeometry::SIZES[label['papersize']]
             raise "Paper size '#{label['papersize']}' not supported" if geom.nil?
-    
+
             @paper_width = geom[0]
             @paper_height = geom[1]
-    
+
             @top_margin = TaskboardCards.topts(label['top_margin'])
             @vertical_pitch = TaskboardCards.topts(label['vertical_pitch'])
             @height = TaskboardCards.topts(label['height'])
-    
+
             @left_margin = TaskboardCards.topts(label['left_margin'])
             @horizontal_pitch = TaskboardCards.topts(label['horizontal_pitch'])
             @width = TaskboardCards.topts(label['width'])
-    
+
             @across = label['across']
             @down = label['down']
-    
+
             @inner_margin = TaskboardCards.topts(label['inner_margin']) || 1.mm
-    
+
             @pdf = Prawn::Document.new(
                 :page_layout => :portrait,
                 :left_margin => 0,
@@ -72,10 +72,10 @@ module Cards
             )
             @pdf.font "DejaVuSans"
 
-    
+
             @cards = 0
         end
-    
+
         def self.measurement(x)
           x = "#{x}pt" if x =~ /[0-9]$/
           return x
@@ -104,7 +104,7 @@ module Cards
              'misc-iso-templates.xml',
              'misc-other-templates.xml',
              'misc-us-templates.xml',
-             'pearl-iso-templates.xml',  
+             'pearl-iso-templates.xml',
              'uline-us-templates.xml',
              'worldlabel-us-templates.xml',
              'zweckform-iso-templates.xml'].each {|filename|
@@ -138,20 +138,20 @@ module Cards
                 end
 
                 doc = REXML::Document.new(labels)
-    
+
                 doc.elements.each('Glabels-templates/Template') do |specs|
                     label = nil
-    
+
                     papersize = specs.attributes['size']
                     papersize = 'Letter' if papersize == 'US-Letter'
-    
+
                     specs.elements.each('Label-rectangle') do |geom|
                         margin = nil
                         geom.elements.each('Markup-margin') do |m|
                             margin = m.attributes['size']
                         end
                         margin = "1mm" if margin.blank?
-    
+
                         geom.elements.each('Layout') do |layout|
                             label = {
                                 'inner_margin' => TaskboardCards.measurement(margin),
@@ -168,9 +168,9 @@ module Cards
                             }
                         end
                     end
-    
+
                     next if label.nil?
-    
+
                     key = "#{specs.attributes['brand']} #{specs.attributes['part']}"
 
                     if TaskboardCards.malformed(label)
@@ -178,7 +178,7 @@ module Cards
                       malformed_labels[key] = label
                     else
                       LABELS[key] = label if not LABELS[key] or LABELS[key]['source'] == 'glabel'
-    
+
                       specs.elements.each('Alias') do |also|
                           key = "#{also.attributes['brand']} #{also.attributes['part']}"
                           LABELS[key] = label.dup if not LABELS[key] or LABELS[key]['source'] == 'glabel'
@@ -186,7 +186,7 @@ module Cards
                     end
                 end
             }
-    
+
             File.open(File.dirname(__FILE__) + '/labels.yaml', 'w') do |dump|
                 YAML.dump(LABELS, dump)
             end
@@ -203,29 +203,29 @@ module Cards
               Setting.plugin_redmine_backlogs = s
             end
         end
-    
+
         attr_reader :pdf
-    
+
         def card(issue, type)
             row = (@cards % @down) + 1
             col = ((@cards / @down) % @across) + 1
             @cards += 1
-    
+
             @pdf.start_new_page if row == 1 and col == 1 and @cards != 1
-    
+
             parent_story = (type == :task ? issue.story : issue)
-    
+
             # card bounds
             @pdf.bounding_box self.top_left(row, col), :width => @width, :height => @height do
                 @pdf.line_width = 0.5
                 @pdf.stroke do
                     @pdf.stroke_bounds
-                    
+
                     # card margin
                     @pdf.bounding_box [@inner_margin, @height - @inner_margin],
                                         :width => @width - (2 * @inner_margin),
                                         :height => @height - (2 * @inner_margin) do
-    
+
                         scoresize = 0
                         @y = @pdf.bounds.height
                         @pdf.font_size(12) do
@@ -233,13 +233,13 @@ module Cards
                             score ||= '?'
                             score = "#{score} #{type == :task ? l(:label_hours) : l(:label_points)}"
                             scoresize = @pdf.width_of(" #{score} ")
-    
+
                             text_box(score, {
                                     :width => scoresize,
                                     :height => @pdf.font.height
                                 }, pdf.bounds.width - scoresize)
                         end
-    
+
                         @y = @pdf.bounds.height
                         pos = parent_story.position ? parent_story.position : l(:label_not_prioritized)
                         trail = (issue.self_and_ancestors.reverse.collect{|i| "#{i.tracker.name} ##{i.id}"}.join(" : ")) + " (#{pos})"
@@ -250,8 +250,8 @@ module Cards
                                     :style => :italic
                                 })
                         end
-    
-    
+
+
                         @pdf.font_size(6) do
                             parent = (type == :task ? parent_story.subject : (issue.fixed_version ? issue.fixed_version.name : I18n.t(:backlogs_product_backlog)))
                             text_box parent, {
@@ -259,14 +259,14 @@ module Cards
                                     :height => @pdf.font.height
                             }
                         end
-    
+
                         text_box issue.subject, {
                                 :width => pdf.bounds.width,
                                 :height => @pdf.font.height * 2
                             }
                         @pdf.line [0, @y], [pdf.bounds.width, @y]
                         @y -= 2
-    
+
                         @pdf.font_size(8) do
                             text_box issue.description || issue.subject , {
                                     :width => pdf.bounds.width,
@@ -275,7 +275,7 @@ module Cards
                         end
 
                         start_of_footer = @y
-                        
+
                         @y = start_of_footer
                         @pdf.font_size(6) do
                             category = issue.category ? "#{l(:field_category)}: #{issue.category.name}" : ''
@@ -285,10 +285,10 @@ module Cards
                                     :height => @pdf.font.height
                                 })
                         end
-                        
+
                         @y = start_of_footer
                         gravatar_offset = gravatar_box(issue)
-                        
+
                         @y = start_of_footer
                         @pdf.font_size(10) do
                             #category = issue.category ? "#{l(:field_category)}: #{issue.category.name}" : ''
@@ -303,38 +303,38 @@ module Cards
                 end
             end
         end
-    
+
         def add(story, add_tasks = true)
             if add_tasks
                 story.descendants.each {|task|
                     card(task, :task)
                 }
             end
-    
+
             card(story, :story)
         end
-         
+
         def gravatar_box(issue)
             return 0 if issue.assigned_to.blank?
-                        
+
             gravatar_width = 20
-            
+
             image_url = gravatar_url(issue.assigned_to.mail.to_s.downcase, :size => gravatar_width * 4)
             image_obj = open(image_url)
             y_offset = @y + (gravatar_width - 10)
             x_offset = @pdf.bounds.width - gravatar_width
             @pdf.image image_obj, :at => [x_offset, y_offset], :width => gravatar_width
-            
+
             return gravatar_width + 2
-        end 
-        
+        end
+
         def text_box(s, options, x = 0)
             box = Prawn::Text::Box.new(s, options.merge(:overflow => :ellipses, :at => [x, @y], :document => @pdf))
             box.render
             @y -= (options[:height] + (options[:size] || @pdf.font_size) / 2)
             return box
         end
-    
+
         def self.topts(v)
             return nil if v.class == NilClass
 
@@ -353,7 +353,7 @@ module Cards
             return Float(v).pt if units == 'pt'
             raise "Unexpected units '#{units}'"
         end
-    
+
         def top_left(row, col)
             top = @paper_height - (@top_margin + @vertical_pitch * (row - 1))
             left = @left_margin + (@horizontal_pitch * (col - 1))
