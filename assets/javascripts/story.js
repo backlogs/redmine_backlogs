c1f01351e1cfb0f82110072e217bdea1b628b53e
friflaj <friflaj@redminebacklogs.net>
2011-07-01 14:54:54 +0200

    Make backlogs screen adhere to workflow rules

--- assets/javascripts/story.js
+++ assets/javascripts/story.js
@@ -24,7 +24,42 @@ RB.Story = RB.Object.create(RB.Issue, RB.EditableInplace, {
   },
 
   editorDisplayed: function(editor){
-    // editor.dialog("option", "position", "center");
+    var tracker = editor.find('.tracker_id.editor');
+    var status = editor.find('.status_id.editor');
+    var self = this;
+
+    this.setAllowedStatuses(tracker, status);
+    tracker.change(function() { self.setAllowedStatuses(tracker, status) });
+  },
+
+  setAllowedStatuses: function(tracker, status) {
+    var tracker_id = tracker.val();
+    var user_status = this.$.find(".user_status").text();
+    var status_id = status.val()
+
+    var states = RB.constants.story_states['transitions'][tracker_id][user_status][status_id];
+    if (!states) { states = RB.constants.story_states['transitions'][tracker_id][user_status][RB.constants.story_states['transitions'][tracker_id][user_status]['default']]; }
+
+    if ($.inArray(status_id, states) == -1) { // a non-available state is currently selected, tracker has changed
+      status_id = null;
+
+      if (this.$.find('.tracker_id .v').text() == tracker_id) { // if we're switching back to the original tracker, select the original state
+        status_id = this.$.find('.status_id .v').text();
+      } else { // pick first available
+        if (states.length != 0) {
+          status_id = states[0];
+        }
+      }
+    }
+
+    status.empty();
+    for (var i = 0; i < states.length; i++) {
+      option = $('<option/>');
+      state = RB.constants.story_states['states'][states[i]];
+      option.attr('value', states[i]).addClass(state.closed).text(state.name);
+      if (states[i] == status_id) { option.attr('selected', 'selected'); }
+      status.append(option);
+    }
   },
   
   getPoints: function(){
