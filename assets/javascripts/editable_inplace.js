cc7b0a203072b2fce421e8146d2a0327c57e1688
friflaj <friflaj@redminebacklogs.net>
2011-07-14 19:19:43 +0200

    Fix for IME input (#126)

--- assets/javascripts/editable_inplace.js
+++ assets/javascripts/editable_inplace.js
@@ -2,7 +2,8 @@ RB.EditableInplace = RB.Object.create(RB.Model, {
 
   displayEditor: function(editor){
     this.$.addClass("editing");
-    editor.find(".editor").bind('keyup', this.handleKeyup);
+    editor.find(".editor").bind('keyup', this.handleKeyup).bind('keydown', this.handleKeydown).bind('keypress', this.handleKeypress);
+    this.isEnter = false;
   },
 
   getEditor: function(){
@@ -16,17 +17,35 @@ RB.EditableInplace = RB.Object.create(RB.Model, {
     return editor;
   },
 
-  handleKeyup: function(event){
+  handleKeydown: function(event) {
+    this.isEnter = (event.which == 13);
+    return true;
+  },
+  handleKeypress: function(event) {
+    this.isEnter = this.isEnter && (event.which == 13);
+    return true;
+  },
+  handleKeyup: function(event) {
     j = $(this).parents('.model').first();
     that = j.data('this');
 
-    switch(event.which){
-      case 13   : that.saveEdits();   // Enter
-                  break;
-      case 27   : that.cancelEdit();     // ESC
-                  break;
-      default   : return true;
+    var isEnter = this.isEnter;
+    this.isEnter = false;
+
+    switch(event.which) {
+      case 13:
+        if (isEnter) { that.saveEdits();
+        } else {
+          return true;
+        }
+        break;
+
+      case 27:
+        that.cancelEdit();     // ESC
+        break;
+
+      default:
+        return true;
     }
   }
-
 });
