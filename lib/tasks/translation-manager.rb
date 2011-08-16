4d9e60aece51436399e0c98cf5436d21e0d2ff28
friflaj <friflaj@redminebacklogs.net>
2011-08-16 14:16:12 +0200

    translations

--- lib/tasks/translation-manager.rb
+++ lib/tasks/translation-manager.rb
@@ -10,6 +10,7 @@ require 'fileutils'
 class Translation
   @@source = nil
   @@translations = {}
+  @@keys = nil
 
   def initialize(source, options={})
     @strings = {}
@@ -22,7 +23,13 @@ class Translation
     options[:register] = true unless options.include?(:register)
     options[:register] = true if options[:source]
 
-    load(source, options)
+    strings = YAML::load_file(source)
+    @lang = strings.keys[0]
+    strings[@lang].each_pair{|k, v| self[k] = v }
+
+    rmtrans = File.expand_path(File.join('..', '..', '..', 'redmine', 'config', 'locales'), File.dirname(__FILE__))
+    rmtrans = File.join(rmtrans, "#{File.basename(source, File.extname(source))}.yml")
+    @name = YAML::load_file(rmtrans)[@lang]['general_lang_name']
 
     raise "Translation '#{@lang}' already registered" if options[:register] && @@translations[@lang]
     @@translations[@lang] = self if options[:register]
@@ -30,58 +37,34 @@ class Translation
     raise "Source re-registered!" if @@source && options[:source]
     if options[:source]
       @@source = self
-      @key_order = {}
+      @@keys = []
 
-      File.open(Translation.source.source).each_with_index {|line, no|
+      File.open(@@source.source).each {|line|
         m = line.match(/^\s+[-_a-z0-9]+\s*:/)
         next unless m
         key = m[0].strip.gsub(/:$/, '').strip
-        @key_order[key] = no
+        @@keys << key
       }
     end
 
     test
   end
 
-  attr_reader :lang, :name, :strings, :source, :key_order
+  attr_reader :lang, :name, :strings, :source
   attr_reader :missing, :obsolete, :varstyle
 
-  def load(source, options)
-    @author = options[:author] if options[:author]
+  def merge(qts)
+    doc = Nokogiri::XML(qts)
 
-    case File.extname(source)
-      when '.qts', '.ts'
-        doc = nil
-        File.open(source) do |f|
-          doc = Nokogiri::XML(f)
-        end
+    lang = doc.at('//TS')['language']
+    raise "Cannot load '#{lang}' over '#{@lang}'" unless @lang == lang
 
-        lang = doc.at('//TS')['language']
-        author = doc.at('//TS')['extra-rbl-author']
-        @author = author if author
-
-        doc.xpath('//message').each {|message|
-          id = message['id']
-          t = message.at('translation')
-          next if t['type'] == 'unfinished'
-          self[id] = t.text
-        }
-
-      when '.yaml', '.yml'
-        strings = YAML::load_file(source)
-        lang = strings.keys[0]
-        strings[lang].each_pair{|k, v| self[k] = v }
-
-      else
-        raise "Unsupported translation format '#{source}'"
-    end
-
-    raise "Cannot load '#{lang}' over '#{@lang}'" if @lang && @lang != lang
-    @lang = lang
-
-    rmtrans = File.expand_path(File.join('..', '..', '..', 'redmine', 'config', 'locales'), File.dirname(__FILE__))
-    rmtrans = File.join(rmtrans, "#{File.basename(source, File.extname(source))}.yml")
-    @name = YAML::load_file(rmtrans)[@lang]['general_lang_name']
+    doc.xpath('//message').each {|message|
+      id = message['id']
+      t = message.at('translation')
+      next if t['type'] == 'unfinished'
+      self[id] = t.text
+    }
   end
 
   def [](k)
@@ -118,6 +101,20 @@ class Translation
     @@source
   end
 
+  def self.keys
+    @@keys
+  end
+
+  def self.cmp(a, b)
+    pa = @@keys.index(a)
+    pb = @@keys.index(b)
+
+    return (pa <=> pb) if pa && pb
+    return pa if pa
+    return pb if pb
+    return a <=> b
+  end
+
   def self.test
     @@source.test if @@source
   end
@@ -127,12 +124,8 @@ class Translation
   end
 
   def to_yaml(opts = {})
-    if @@source
-      strings = {}
-      @@source.keys.each {|k| strings[k] = @strings[k]}
-    else
-      strings = @strings
-    end
+    strings = {}
+    @@keys.each {|k| strings[k] = @strings[k]}
     return {@lang => strings}.to_yaml(opts)
   end
   
@@ -142,14 +135,10 @@ class Translation
       xml.TS({'sourcelanguage' => @@source.lang.gsub('-', '_'), 'language' => @lang.gsub('-', '_')}.merge(@author ? {'extra-rbl-author' => @author} : {})) {
         xml.context_ {
           xml.name('Redmine Backlogs')
-          @@source.strings.each_pair {|id, str|
+          @@keys.each {|id|
             xml.message('id' => id) {
-              xml.source(str)
-
-              attrs = {}
-              attrs['type'] = 'unfinished' if @varstyle.include?(id) || @missing.include?(id)
-              xml.translation(@strings[id] || '', attrs)
-
+              xml.source(@@source.strings[id])
+              xml.translation(@strings[id] || '', {}.merge(@varstyle.include?(id) || @missing.include?(id) ? {'type' => 'unfinished'} : {}))
               xml.translatorcomment('Please replace {{...}} variables with %{...} variables') if @varstyle.include?(id)
             }
           }
@@ -166,50 +155,12 @@ class Translation
 
     return builder.to_xml
   end
-
-  def to_xliff
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml.xliff(:version => '1.2', 'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance", 'xsi:schemaLocation' => 'urn:oasis:names:tc:xliff:document:1.2 xliff-core-1.2-transitional.xsd') {
-        xml.file(:original => 'Redmine Backlogs', "source-language" => @@source.lang, "target-language" => @lang, 'datatype' =>'plaintext') {
-          xml.header
-          xml.body {
-            @@source.strings.each_pair {|id, str|
-              xml.send(:"trans-unit", :id => id) {
-                attr = {'xml:lang' => @lang}
-                if @missing.include?(id)
-                  attr['state'] = 'needs-translation'
-                elsif @varstyle.include?(id)
-                  attr['state'] = 'needs-adaptation'
-                else
-                  attr['state'] = 'final'
-                end
-
-                xml.source(str, 'xml:lang' => 'en')
-
-                xml.target(@strings[id] || str, attr)
-
-                xml.note('Needs translation') if @missing.include?(id)
-                xml.note('Uses {{...}} variable substitution, please change to %{...}') if @varstyle.include?(id)
-              }
-            }
-            #@obsolete.each {|id|
-            #  xml.send(:"trans-unit", :id => id) {
-            #    xml.source("Obsolete key '#{id}' -- use for reference, or delete", 'xml:lang' => 'en')
-            #    xml.target(@strings[id], 'xml:lang' => @lang, 'state' => 'needs-review-translation')
-            #    xml.note('Obsolete -- only kept for reference')
-            #  }
-            #}
-          }
-        }
-      }
-     end
-
-     return builder.to_xml
-  end
-
 end
 
 class TranslationManager
+  @@configfile = File.join(File.dirname(__FILE__), File.basename(__FILE__, File.extname(__FILE__))) + '.rc'
+  @@config = File.exists?(@@configfile) ? YAML::load(File.open(@@configfile)) : {}
+
   def initialize
     @root = File.expand_path(File.join('..', '..', '..'), File.dirname(__FILE__))
     @webdir = File.join(@root, 'www.redminebacklogs.net')
@@ -227,9 +178,6 @@ class TranslationManager
     }
 
     raise "Source translation 'en' not found" unless Translation.source
-  end
-
-  def load_submissions
   end
 
   def save
@@ -331,15 +279,9 @@ end
 class Hash
   # sorted keys for cleaner diffs in git
   def to_yaml(opts = {})
-    if Translation.source
-      o = Translation.source.key_order
-    else
-      o = {}
-    end
-
     YAML::quick_emit(object_id, opts) do |out|
       out.map(taguri, to_yaml_style) do |map|
-        sort{|a, b| (o[a] && o[b]) ? (o[a] <=> o[b]) : (o[a] ? 1 :(o[b] ? -1 : a<=>b)) }.each do |k, v|
+        sort{|a, b| Translation.cmp(a, b) }.each do |k, v|
           map.add(k, v)
         end
       end
