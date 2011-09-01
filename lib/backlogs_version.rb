629a2479b477b160723b98c8a6620725aefe9fdc
friflaj <friflaj@redminebacklogs.net>
2011-09-01 20:57:46 +0200

    version

--- lib/backlogs_version.rb
+++ lib/backlogs_version.rb
@@ -4,19 +4,23 @@ module Backlogs
     git = File.join(root, '.git')
     changelog = File.join(root, 'CHANGELOG')
     v = nil
+    File.open(changelog).readlines.each do |l|
+      m = l.match(/^== [0-9]{4}-[0-9]{2}-[0-9]{2}\s+v(.+)/)
+      next unless m
+      v = m[1]
+      break
+    end
+
+    g = nil
     if File.directory?(git)
       Dir.chdir(root)
-      v = `git log | head -1 | awk '{print $2}'`
-      v.strip!
-      v = "git: #{v}"
-    else
-      File.open(changelog).readlines.each do |l|
-        m = l.match(/^== [0-9]{4}-[0-9]{2}-[0-9]{2}\s+v(.+)/)
-        next unless m
-        v = m[1]
-        break
-      end
+      g = `git log | head -1 | awk '{print $2}'`
+      g.strip!
+      g = "(#{g})"
     end
+
+    v = [v, g].compact.join(' ')
+    v = '?' if v == ''
     return v
   end
 
