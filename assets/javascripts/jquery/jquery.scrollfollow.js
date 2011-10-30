adc33b70348230621402c973d48bda3015ffdea6
mikoto20000 <mikoto20000@gmail.com>
2011-10-30 09:38:40 +0900

    Fixed jquery.scrollfollow doesn't work with jquery 1.6.

--- assets/javascripts/jquery/jquery.scrollfollow.js
+++ assets/javascripts/jquery/jquery.scrollfollow.js
@@ -55,8 +55,8 @@
 			var viewportHeight = parseInt( $( window ).height() );	
 			var pageScroll =  parseInt( $( document ).scrollTop() );
 			var parentTop =  parseInt( box.cont.offset().top );
-			var parentHeight = parseInt( box.cont.attr( 'offsetHeight' ) );
-			var boxHeight = parseInt( box.attr( 'offsetHeight' ) + ( parseInt( box.css( 'marginTop' ) ) || 0 ) + ( parseInt( box.css( 'marginBottom' ) ) || 0 ) );
+			var parentHeight = parseInt(box.cont.css('padding-top')) + parseInt(box.cont.css('height')); 
+			var boxHeight = parseInt( (parseInt(box.css('padding-top')) + parseInt(box.css('height'))) + ( parseInt( box.css( 'marginTop' ) ) || 0 ) + ( parseInt( box.css( 'marginBottom' ) ) || 0 ) );			
 			var aniTop;
 			
 			// Make sure the user wants the animation to happen
