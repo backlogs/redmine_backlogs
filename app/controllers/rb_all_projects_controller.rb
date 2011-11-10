cad9db6912481aa5da5f11273e991e19bd0cdbc2
friflaj <friflaj@redminebacklogs.net>
2011-11-10 12:42:42 +0100

    Route cleanup, server_variables cleanup, potential fix for #327

--- app/controllers/rb_all_projects_controller.rb
+++ app/controllers/rb_all_projects_controller.rb
@@ -1,9 +1,9 @@
-class RbStatisticsController < ApplicationController
+class RbAllProjectsController < ApplicationController
   unloadable
 
   before_filter :authorize_global
 
-  def show
+  def statistics
     @projects = EnabledModule.find(:all,
                                 :conditions => ["enabled_modules.name = 'backlogs' and status = ?", Project::STATUS_ACTIVE],
                                 :include => :project,
@@ -11,4 +11,9 @@ class RbStatisticsController < ApplicationController
     @projects.sort! {|a, b| a.scrum_statistics.score <=> b.scrum_statistics.score}
   end
 
+  def server_variables
+    respond_to do |format|
+      format.js { render :file => 'rb_server_variables/show.js.erb', :layout => false }
+    end
+  end
 end
