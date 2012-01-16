f9b572d286e309950c91863d63bf91efde54b822
friflaj <friflaj@redminebacklogs.net>
2012-01-16 20:47:11 +0100

    v0.7.7

--- init.rb
+++ init.rb
@@ -35,7 +35,7 @@ Redmine::Plugin.register :redmine_backlogs do
   name 'Redmine Backlogs'
   author 'relaxdiego, friflaj'
   description 'A plugin for agile teams'
-  version 'v0.7.6'
+  version 'v0.7.7'
 
   settings :default => { 
                          :story_trackers            => nil, 
