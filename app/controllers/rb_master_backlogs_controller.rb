8f949fc7e248f1a006a9a73b1c1a3f4010cc2619
drakontia <mhden@drakontia.com>
2012-04-26 01:43:48 +0900

    Redmine 1.4.1 & Ruby 1.9.3 support

--- app/controllers/rb_master_backlogs_controller.rb
+++ app/controllers/rb_master_backlogs_controller.rb
@@ -15,8 +15,8 @@ class RbMasterBacklogsController < RbApplicationController
     end
 
     last_story = RbStory.find(
-                          :first, 
-                          :conditions => ["project_id=? AND tracker_id in (?)", @project, RbStory.trackers],
+                          :first,
+                          :conditions => ["project_id=? AND tracker_id in (?)", @project.id, RbStory.trackers],
                           :order => "updated_on DESC"
                           )
     @last_update = (last_story ? last_story.updated_on : nil)
@@ -24,7 +24,7 @@ class RbMasterBacklogsController < RbApplicationController
     sprints_backlog_storie_of = RbStory.backlogs_by_sprint(@project, [sprints, c_sprints].flatten)
     @sprint_backlogs = sprints.map{ |s| { :sprint => s, :stories => sprints_backlog_storie_of[s.id] } }
     @c_sprint_backlogs = c_sprints.map{|s| { :sprint => s, :stories => sprints_backlog_storie_of[s.id] } }
-    
+
     respond_to do |format|
       format.html { render :layout => "rb"}
     end
@@ -44,22 +44,22 @@ class RbMasterBacklogsController < RbApplicationController
               :classname => 'show_burndown_chart'
              } if @sprint && @sprint.stories.size > 0 && @sprint.has_burndown?
     links << {:label => l(:label_stories_tasks),
-              :url => url_for(:controller => 'rb_queries', :action => 'show', :project_id => @project, :sprint_id => @sprint, :only_path => true)
+              :url => url_for(:controller => 'rb_queries', :action => 'show', :project_id => @project.id, :sprint_id => @sprint, :only_path => true)
              } if @sprint && @sprint.stories.size > 0
     links << {:label => l(:label_stories),
               :url => url_for(:controller => 'rb_queries', :action => 'show', :project_id => @project, :only_path => true)
              } unless @sprint
     links << {:label => l(:label_sprint_cards),
-              :url => url_for(:controller => 'rb_stories', :action => 'index', :project_id => @project.identifier, :sprint_id => @sprint, :only_path => true)
+              :url => url_for(:controller => 'rb_stories', :action => 'index', :project_id => @project.identifier, :sprint_id => @sprint, :format => 'pdf', :only_path => true)
              } if @sprint && BacklogsPrintableCards::CardPageLayout.selected && @sprint.stories.size > 0
     links << {:label => l(:label_product_cards),
-              :url => url_for(:controller => 'rb_stories', :action => 'index', :project_id => @project.identifier, :only_path => true)
+              :url => url_for(:controller => 'rb_stories', :action => 'index', :project_id => @project.identifier, :format => 'pdf', :only_path => true)
              } unless @sprint
     links << {:label => l(:label_wiki),
               :url => url_for(:controller => 'rb_wikis', :action => 'edit', :project_id => @project.id, :sprint_id => @sprint, :only_path => true)
              } if @sprint && @project.enabled_modules.any? {|m| m.name=="wiki" }
     links << {:label =>  l(:label_download_sprint),
-              :url => url_for(:controller => 'rb_sprints', :action => 'download', :sprint_id => @sprint, :only_path => true)
+              :url => url_for(:controller => 'rb_sprints', :action => 'download', :sprint_id => @sprint, :format => 'xml', :only_path => true)
              } if @sprint && @sprint.has_burndown?
     links << {:label => l(:label_reset),
               :url => url_for(:controller => 'rb_sprints', :action => 'reset', :sprint_id => @sprint, :only_path => true),
