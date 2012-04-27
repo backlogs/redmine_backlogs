79696f5a9e2d71d81ef8793d06c0374bd62541cd
Igor Zubkov <icesik@altlinux.org>
2012-04-27 16:57:11 +0300

    Fix indent

--- lib/backlogs_issues_controller_patch.rb
+++ lib/backlogs_issues_controller_patch.rb
@@ -5,13 +5,13 @@ require 'json'
 module Backlogs
   module IssuesControllerPatch
     def self.included(base) # :nodoc:
-        base.extend(ClassMethods)
-        base.send(:include, InstanceMethods)
+      base.extend(ClassMethods)
+      base.send(:include, InstanceMethods)
 
-        base.class_eval do
-          unloadable # Send unloadable so it will not be unloaded in development
-          after_filter :add_backlogs_fields, :only => [:index]
-        end
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+        after_filter :add_backlogs_fields, :only => [:index]
+      end
     end
 
     module ClassMethods
