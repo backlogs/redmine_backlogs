8962f60ea93a091f1a7bbafa1cbd9000456589d9
John Yani <vanuan@gmail.com>
2012-05-10 23:29:56 +0300

    Add travis ci support

--- redmine20_install.sh
+++ redmine20_install.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-if [ ! -n "${WORKSPACE}" ] ||
-   [ ! -n "${PATH_TO_REDMINE}" ] ||
-   [ ! -n "${PATH_TO_BACKLOGS}" ];
+if [[ ! "$WORKSPACE" = /* ]] ||
+   [[ ! "$PATH_TO_REDMINE" = /* ]] ||
+   [[ ! "$PATH_TO_BACKLOGS" = /* ]];
 then
   echo "You should set"\
        " WORKSPACE, PATH_TO_REDMINE, PATH_TO_BACKLOGS"\
@@ -11,30 +11,35 @@ then
 fi
 
 # cd to redmine folder
+echo changing directory to $PATH_TO_REDMINE
 cd $PATH_TO_REDMINE
+echo current directory is `pwd`
 
 # create a link to the backlogs plugin
-ln -sf ../$PATH_TO_BACKLOGS plugins/redmine_backlogs
+echo creating a symbolic link ./plugins/redmine_backlogs to $PATH_TO_BACKLOGS
+ln -sf $PATH_TO_BACKLOGS ./plugins/redmine_backlogs
 
 # install gems
+export BUNDLE_GEMFILE=$PATH_TO_REDMINE/Gemfile
+mkdir -p vendor/bundle
 bundle install --path vendor/bundle
 
 # copy database.yml
 cp $WORKSPACE/database.yml config/
 
 # run redmine database migrations
-rake db:migrate RAILS_ENV=test --trace
-rake db:migrate RAILS_ENV=development --trace
+bundle exec rake db:migrate RAILS_ENV=test --trace
+bundle exec rake db:migrate RAILS_ENV=development --trace
 
 # install redmine database
-REDMINE_LANG=en RAILS_ENV=development rake redmine:load_default_data
+bundle exec rake redmine:load_default_data REDMINE_LANG=en RAILS_ENV=development
 
 # install backlogs
-rake redmine:backlogs:install labels=no story_trackers=Story task_tracker=Task override_unsupported=true RAILS_ENV=development --trace
+bundle exec rake redmine:backlogs:install labels=no story_trackers=Story task_tracker=Task override_unsupported=true RAILS_ENV=development --trace
 
 # run backlogs database migrations
-rake redmine:plugins:migrate RAILS_ENV=test
-rake redmine:plugins:migrate RAILS_ENV=development
+bundle exec rake redmine:plugins:migrate RAILS_ENV=test
+bundle exec rake redmine:plugins:migrate RAILS_ENV=development
 
 # create a link to cucumber features
 ln -sf $PATH_TO_BACKLOGS/features/ .
@@ -49,6 +54,6 @@ ln -sf `pwd`/coverage $WORKSPACE
 bundle exec cucumber -f junit --out $WORKSPACE
 
 # clean up database
-rake redmine:plugins:migrate NAME=redmine_backlogs VERSION=0 RAILS_ENV=test
-rake redmine:plugins:migrate NAME=redmine_backlogs VERSION=0 RAILS_ENV=development
+bundle exec rake redmine:plugins:migrate NAME=redmine_backlogs VERSION=0 RAILS_ENV=test
+bundle exec rake redmine:plugins:migrate NAME=redmine_backlogs VERSION=0 RAILS_ENV=development
 
