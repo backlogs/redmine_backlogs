bfcb5b550df5d49645b7ef235a3a44f752daa9d5
friflaj <friflaj@redminebacklogs.net>
2012-05-24 11:50:40 +0200

    Removed obsolete migration

--- db/migrate/033_unique_positions.rb
+++ db/migrate/033_unique_positions.rb
@@ -2,35 +2,17 @@ require 'benchmark'
 
 class UniquePositions < ActiveRecord::Migration
   def self.up
-    execute("drop table if exists _backlogs_tmp_position")
-
-    execute("create table _backlogs_tmp_position (issue_id int not null unique, new_position int not null unique)")
-
-    execute("
-      insert into _backlogs_tmp_position (issue_id, new_position)
-      select id, (
-        select count(*)
-        from issues pred
-        where
-        (pred.position is not null and story.position is not null and pred.position < story.position)
-        or
-        (pred.position is not null and story.position is not null and pred.position = story.position and pred.id < story.id)
-        or
-        (story.position is null and pred.position is not null)
-        or
-        (story.position is null and pred.position is null and pred.id < story.id)
-      )
-      from issues story
-    ")
-
-    execute("update issues set position = (select new_position from _backlogs_tmp_position where id = issue_id)")
-    execute("drop table _backlogs_tmp_position")
+    RbStory.transaction do
+      ids = RbStory.connection.select_values('select id from issues order by position')
+      ids.each_with_index{|id, i|
+        RbStory.connection.execute("update issues set position = #{i * RbStory::POSITION_GAP} where id = #{id}")
+      }
+    end
 
     change_column :issues, :position, :integer, :null => false
 
     # Needed until MySQL undoes the retardation that is http://bugs.mysql.com/bug.php?id=5573
     add_column :issues, :position_lock, :integer, :null=>false, :default => 0
-
     add_index :issues, [:position, :position_lock], :unique => true
   end
 
--- lib/tasks/fix_positions.rake
+++ lib/tasks/fix_positions.rake
@@ -6,7 +6,6 @@ namespace :redmine do
         puts "Please run plugin migrations first"
       else
         RbStory.connection.execute("drop table if exists _backlogs_tmp_position")
-        RbStory.connection.execute("create table _backlogs_tmp_position (issue_id int not null unique, new_position int not null unique)")
 
         RbStory.transaction do
           RbStory.connection.execute("update issues set position_lock=position")
