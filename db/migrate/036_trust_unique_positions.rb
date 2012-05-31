e1c87e2f4936b53e68b56a9ae15d0dc8bd0e5448
friflaj <friflaj@redminebacklogs.net>
2012-05-31 11:16:45 +0200

    down migration

--- db/migrate/036_trust_unique_positions.rb
+++ db/migrate/036_trust_unique_positions.rb
@@ -2,13 +2,13 @@ require 'benchmark'
 
 class TrustUniquePositions < ActiveRecord::Migration
   def self.up
-    change_column :issues, :position, :integer, :null => false
-
     # Needed until MySQL undoes the retardation that is http://bugs.mysql.com/bug.php?id=5573
     remove_index :issues, [:position, :position_lock]
     remove_column :issues, :position_lock
   end
 
   def self.down
+    add_column :issues, :position_lock
+    add_index :issues, [:position, :position_lock]
   end
 end
