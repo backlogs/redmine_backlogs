a6a67d20a8d7e957ddcf28b3c1b39d57cd5a0227
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2012-06-08 22:09:02 +0200

    note deprecated jquery call

--- assets/javascripts/story.js
+++ assets/javascripts/story.js
@@ -12,7 +12,7 @@ RB.Story = RB.Object.create(RB.Issue, RB.EditableInplace, {
     // Associate this object with the element for later retrieval
     j.data('this', this);
 
-    j.find(".editable").live('mouseup', this.handleClick);
+    j.find(".editable").live('mouseup', this.handleClick); //live is deprecated! use .on or .delegate
   },
 
   afterUpdate: function(data, textStatus, xhr){
