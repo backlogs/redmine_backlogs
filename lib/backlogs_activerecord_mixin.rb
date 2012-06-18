00c4d8dd8bdfa6e845dbe1b6c4ccc86a634d7399
friflaj <friflaj@redminebacklogs.net>
2012-06-19 01:07:06 +0200

    Fixes #625

--- lib/backlogs_activerecord_mixin.rb
+++ lib/backlogs_activerecord_mixin.rb
@@ -27,7 +27,8 @@ module Backlogs
       end
 
       def available_custom_fields
-        CustomField.find(:all, :conditions => "type = '#{self.class.rb_sti_class.name}CustomField'", :order => 'position')
+        klass = self.class.respond_to?(:rb_sti_class) ? self.class.rb_sti_class : self.class
+        CustomField.find(:all, :conditions => "type = '#{klass.name}CustomField'", :order => 'position')
       end
 
       def journalized_update_attributes!(attribs)
