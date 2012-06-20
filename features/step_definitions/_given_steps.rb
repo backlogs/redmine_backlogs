7d2c872732ad7fae667257230fea1a29a4c75cde
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2012-06-20 09:03:31 +0200

    give product owner modify_releases permission

--- features/step_definitions/_given_steps.rb
+++ features/step_definitions/_given_steps.rb
@@ -7,9 +7,7 @@ Given /^I am a product owner of the project$/ do
   role.permissions << :create_stories
   role.permissions << :update_stories
   role.permissions << :view_releases
-  role.permissions << :create_releases
-  role.permissions << :update_releases
-  role.permissions << :destroy_releases
+  role.permissions << :modify_releases
   role.permissions << :view_scrum_statistics
   role.save!
   login_as_product_owner
