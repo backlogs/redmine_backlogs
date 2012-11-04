cc3eb42b19aed4a1a485a08ffe6a4d14ccb65d1f
Bo Hansen <bo@fpgadesign.dk>
2012-11-04 19:46:29 +0100

    Hide projects from scrum statistics if they are not visible for current user + added project setting to show/hide a project from scrum statistics.

--- app/controllers/rb_project_settings_controller.rb
+++ app/controllers/rb_project_settings_controller.rb
@@ -6,11 +6,14 @@ class RbProjectSettingsController < RbApplicationController
 
   def project_settings
     enabled = false
-    if request.post? and params[:settings] and params[:settings]["show_stories_from_subprojects"]=="enabled"
-      enabled = true
+    enabled_scrum_stats = false
+    if request.post? and params[:settings]
+      enabled = true if params[:settings]["show_stories_from_subprojects"]=="enabled"
+      enabled_scrum_stats = true if params[:settings]["show_in_scrum_stats"]=="enabled"
     end
     settings = @project.rb_project_settings
     settings.show_stories_from_subprojects = enabled
+    settings.show_in_scrum_stats = enabled_scrum_stats
     if settings.save
       flash[:notice] = t(:rb_project_settings_updated)
     else
