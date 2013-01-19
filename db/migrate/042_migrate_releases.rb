031e92fe65ef9d2a9f77995802c8e7258c3a054b
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-01-19 20:00:01 +0100

    do not drop old release table/colum for now

--- db/migrate/042_migrate_releases.rb
+++ db/migrate/042_migrate_releases.rb
@@ -7,13 +7,15 @@ class MigrateReleases < ActiveRecord::Migration
     RbRelease.integrate_implicit_stories
     puts "Migration of implicit releases done."
 
-    if ActiveRecord::Base.connection.table_exists?('release_burndown_days')
-      drop_table :release_burndown_days
-    end
-    remove_column :releases, :initial_story_points
+# Do this when its clear that we abandon the old system
+#    if ActiveRecord::Base.connection.table_exists?('release_burndown_days')
+#      drop_table :release_burndown_days
+#    end
+#    remove_column :releases, :initial_story_points
   end
 
   def self.down
-    puts "Reverting irreversible migration"
+    remove_columns :releases, :status
+    remove_columns :releases, :description
   end
 end
