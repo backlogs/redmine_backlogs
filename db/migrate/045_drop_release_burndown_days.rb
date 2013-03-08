52dab9cc030cb9f841fd74b93d0ff10d0b11496a
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-03-08 16:03:18 +0000

    split migrations, do not combine two distinct issues into one

--- db/migrate/045_drop_release_burndown_days.rb
+++ db/migrate/045_drop_release_burndown_days.rb
@@ -4,23 +4,9 @@ class DropReleaseBurndownDays < ActiveRecord::Migration
       drop_table :release_burndown_days
     end
     remove_column :releases, :initial_story_points
-
-    add_index :issues, :release_id
-    add_index :issues, :release_relationship
-    add_index :releases, :project_id
-    add_index :releases, :status
-    add_index :releases, :release_start_date
-    add_index :releases, :release_end_date
   end
 
   def self.down
-    remove_index :issues, :column => :release_id
-    remove_index :issues, :column => :release_relationship
-    remove_index :releases, :column => :project_id
-    remove_index :releases, :column => :status
-    remove_index :releases, :column => :release_start_date
-    remove_index :releases, :column => :release_end_date
-
     create_table :release_burndown_days do |t|
       t.column :release_id, :integer, :null => false
       t.column :day, :date, :null => false
