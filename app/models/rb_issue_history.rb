dda814c7590f462dccf9e54f99160431648c8905
Bo Hansen <bo@fpgadesign.dk>
2013-03-19 08:29:55 +0100

    Adjust filter_release to use first possible closed information either during sprint or after

--- app/models/rb_issue_history.rb
+++ app/models/rb_issue_history.rb
@@ -61,7 +61,7 @@ class RbIssueHistory < ActiveRecord::Base
     if self.issue.status.is_closed? && !self.issue.fixed_version.nil?
       # get closed history sorted by date
       #FIXME wishlist: history table column expansion to allow select and order by date
-      h_closed = self.history.select{|h| h[:date] >= self.issue.fixed_version.effective_date}.collect{|d| [d[:date],d]}.sort{|a,b| a[0] <=> b[0]}
+      h_closed = self.history.select{|h| h[:date] >= self.issue.fixed_version.sprint_start_date}.collect{|d| [d[:date],d]}.sort{|a,b| a[0] <=> b[0]}
       h_closed.each{|h|
         if !h[1][:status_open]
           closed_in_sprint = { :date => self.issue.fixed_version.effective_date, :history => h[1] }
