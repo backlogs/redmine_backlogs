966fba2057c43b47950d3ba907c2eb7232086401
Bo Hansen <bo@fpgadesign.dk>
2013-06-02 13:49:02 +0200

    Change release sidebar links to lead to an issue query. See #865

--- app/controllers/rb_queries_controller.rb
+++ app/controllers/rb_queries_controller.rb
@@ -4,11 +4,17 @@ class RbQueriesController < RbApplicationController
   def show
     @query = __IssueQueryClass.new(:name => "_")
     @query.project = @project
+    group_by = nil
 
     if params[:sprint_id]
       @query.add_filter("status_id", '*', ['']) # All statuses
       @query.add_filter("fixed_version_id", '=', [params[:sprint_id]])
       @query.add_filter("backlogs_issue_type", '=', ['any'])
+    elsif params[:release_id]
+      @query.add_filter("status_id", '*', ['']) # All status
+      @query.add_filter("release_id", '=', [params[:release_id]])
+      @query.add_filter("backlogs_issue_type", '=', ['story'])
+      group_by = 'fixed_version'
     else
       @query.add_filter("status_id", 'o', ['']) # only open
       @query.add_filter("fixed_version_id", '!*', ['']) # only unassigned
@@ -18,7 +24,7 @@ class RbQueriesController < RbApplicationController
     column_names = @query.columns.collect{|col| col.name}
     column_names = column_names + ['position'] unless column_names.include?('position')
 
-    session[:query] = {:project_id => @query.project_id, :filters => @query.filters, :column_names => column_names}
+    session[:query] = {:project_id => @query.project_id, :filters => @query.filters, :column_names => column_names, :group_by => group_by}
     redirect_to :controller => 'issues', :action => 'index', :project_id => @project.id, :sort => 'position'
   end
 
