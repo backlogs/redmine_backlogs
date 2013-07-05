6ba60438f162cdaabc03032d91e9ef571ad92746
Bo Hansen <bo@fpgadesign.dk>
2013-07-05 07:55:52 +0200

    Bugfix for only showing visible projects in scrum statistics. Possibly related to #893.

--- app/controllers/rb_all_projects_controller.rb
+++ app/controllers/rb_all_projects_controller.rb
@@ -7,7 +7,7 @@ class RbAllProjectsController < ApplicationController
     backlogs_projects = RbCommonHelper.find_backlogs_enabled_active_projects
     @projects = []
     backlogs_projects.each{|p|
-      @projects << p unless p.visible?.nil? || p.rb_project_settings.show_in_scrum_stats == false
+      @projects << p if p.visible? && p.rb_project_settings.show_in_scrum_stats
     }
     @projects.sort! {|a, b| a.scrum_statistics.score <=> b.scrum_statistics.score}
   end
