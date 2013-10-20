db5d158c6e7f8f79104c7ea6deb21318f7d52e4b
ande3577 <ande3577@gmail.com>
2013-10-20 09:48:43 -0500

    Added backlogs_issue_type as column.

--- lib/backlogs_issue_query_patch.rb
+++ lib/backlogs_issue_query_patch.rb
@@ -29,6 +29,7 @@ module Backlogs
         base.add_available_column(QueryColumn.new(:position, :sortable => "#{Issue.table_name}.position"))
         base.add_available_column(QueryColumn.new(:remaining_hours, :sortable => "#{Issue.table_name}.remaining_hours"))
         base.add_available_column(QueryColumn.new(:release, :sortable => "#{RbRelease.table_name}.name", :groupable => true))
+        base.add_available_column(QueryColumn.new(:backlogs_issue_type))
 
         alias_method_chain :available_filters, :backlogs_issue_type
         alias_method_chain :sql_for_field, :backlogs_issue_type
