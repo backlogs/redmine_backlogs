724783347aad6b8c6bec680cf5993e4de9686037
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-09-02 14:25:58 -0300

    updated taskboard functions and helpers

--- app/helpers/rb_common_helper.rb
+++ app/helpers/rb_common_helper.rb
@@ -18,7 +18,7 @@ module RbCommonHelper
     status.blank? ? "" : (
         FileTest.exist?("#{Rails.root}/public/images/#{status.name}.jpg") ?
       image_tag("#{status.name}.jpg",  
-          :width => 90, :heigth => 90) : "")
+          :width => 80, :heigth => 80) : "")
 
   end
 
