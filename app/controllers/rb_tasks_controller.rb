1834a121fda5b4883d0cd7e839c1d2f56988be65
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-11-12 14:17:29 -0200

    correção #1362

--- app/controllers/rb_tasks_controller.rb
+++ app/controllers/rb_tasks_controller.rb
@@ -29,11 +29,44 @@ class RbTasksController < RbApplicationController
     status = (result ? 200 : 400)
     @include_meta = true
 
-    @task.story.story_follow_task_state if @task.story
+    #@task.story.story_follow_task_state if @task.story
+
+    settings = Setting['plugin_redmine_issue_status']
+    puts "\n\n\n\n\n settings #{settings}"
+    puts "\n\n\n\n\ #{settings} #{@task.parent} #{(@task.status)} #{(@task.status_was)}"
+    
+    puts "\n\n\nprojeto configurado? #{settings['projects_list'].include? @task.project.id.to_s}"
+
+    verify_children @task, settings if (@task.project && (settings['projects_list'].include? @task.project.id.to_s))
 
     respond_to do |format|
       format.html { render :partial => "task", :object => @task, :status => status }
     end
   end
+
+  def verify_children(issue,settings)
+
+      if issue.parent && issue.parent.children
+      
+          status = issue.status
+          issue.parent.children.each do |c|         
+            if (settings['at_least_statuses_list'].include? c.status.id.to_s)
+              issue.parent.status = c.status
+              issue.parent.save   
+              return
+            end
+            status = c.status if c.status.position < status.position
+          end 
+
+          if (settings['minor_statuses_list'].include? status.id.to_s) && (issue.parent.status != status)
+            puts "Menor status #{status}"
+            issue.parent.status = status
+            issue.parent.save
+
+          end
+
+      end
+
+    end
 
 end
