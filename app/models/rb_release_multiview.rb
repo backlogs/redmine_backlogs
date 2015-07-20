be95d97a8103188f95412836fe6036500c620c7b
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2015-07-20 23:41:00 +0000

    fix rb_release_multiview create

--- app/models/rb_release_multiview.rb
+++ app/models/rb_release_multiview.rb
@@ -2,9 +2,9 @@ class RbReleaseMultiview < ActiveRecord::Base
   self.table_name = 'rb_releases_multiview'
 
   unloadable
-
   belongs_to :project
 
+  attr_accessible :name, :project_id, :release_ids
   serialize :release_ids
 
   validates_presence_of :project_id, :name
