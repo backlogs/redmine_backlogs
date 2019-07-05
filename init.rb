926ad077858e2786028d70496219a2250103851f
y-matsuda <matsuda@lab.acs-jp.com>
2019-07-05 10:37:53 +0900

    bump up version

--- init.rb
+++ init.rb
@@ -46,7 +46,7 @@ Redmine::Plugin.register :redmine_backlogs do
   name 'Redmine Backlogs'
   author "friflaj,Mark Maglana,John Yani,mikoto20000,Frank Blendinger,Bo Hansen,stevel,Patrick Atamaniuk"
   description 'A plugin for agile teams'
-  version 'v1.0.6'
+  version '1.1.0'
 
   settings :default => {
                          :story_trackers            => nil,
