7c16b527892bbcefdec3183da5e1797751d60191
ichylinux <ichylinux@gmail.com>
2020-06-09 22:22:35 +0900

    bump up version

--- init.rb
+++ init.rb
@@ -49,7 +49,7 @@ Redmine::Plugin.register :redmine_backlogs do
   name 'Redmine Backlogs'
   author "friflaj,Mark Maglana,John Yani,mikoto20000,Frank Blendinger,Bo Hansen,stevel,Patrick Atamaniuk"
   description 'A plugin for agile teams'
-  version 'v1.2.3'
+  version 'v1.2.4'
 
   settings :default => {
                          :story_trackers            => nil,
